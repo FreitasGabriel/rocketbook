5165ed3f5e25776bfe688ef542de9639213fba08
gabriel_fs <gabriel.freitas@bemobi.com.br>
2019-07-02 10:21:41 -0300

    adding postHeader

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
+import PostHeader from './PostHeader'
 
 export default class Post extends Component {
     render() {
         return(
-            <Fragment>
-                
-                <p></p>
-            </Fragment>
+            <div className='post'>
+                <PostHeader />
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. It was popularised in 
+                the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                and more recently with desktop publishing software like Aldus PageMaker including 
+                versions of Lorem Ipsum.</p>
+            </div>
         )
     }
 }
 
-render(<Post />, document.getElementById("app_body"))
+render(<Post />, document.getElementById("app"))
---
+++ src/components/PostHeader.js
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from 'react';
+import { render } from 'react-dom';
+
+
+export default class PostHeader extends Component{
+    render() {
+        return (
+            <div className='post_header'>
+                <img src="../image/photo-icon.jpg"></img>
+                <hr id='post_header_hr'></hr>
+            </div>
+        )
+    }
+}
+
+render(<PostHeader/>, document.getElementById('app'))
